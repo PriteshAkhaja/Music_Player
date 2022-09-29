01a2de9856b8c13c11fa22df5ffcbd8b39e828d6
Krushang Kasundra <kruzkasu223@gmail.com>
2022-09-29 22:07:45 +0530

    feat: added feature to rotate song cover on play

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,9 +1,17 @@
 import React from "react"
 
-const Song = ({ currentSong }) => {
+const Song = ({ currentSong, songInfo }) => {
+  const songAnimation = {
+    transform: `rotate(${songInfo.currentTime * 10}deg)`,
+  }
+
   return (
     <div className="song-container">
-      <img alt={currentSong.name} src={currentSong.cover}></img>
+      <img
+        alt={currentSong.name}
+        src={currentSong.cover}
+        style={songAnimation}
+      ></img>
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
